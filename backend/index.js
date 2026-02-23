40764b89eee362aaaf2d24d38ba217e100c13da6
dbadhikari <dipuadhikari1819@gmail.com>
2026-02-23 11:29:47 +0545

    updated

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 connectdb()
 const frontend={
-    origin:"http://localhost:5173",
+    origin:"https://blog-website-1-yee3.onrender.com",
     credentials:true
 }
 app.use(cookieParser())
